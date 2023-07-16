f4d58d0e1d3e443482ce37791d76bee24d03d79b
Bohdan99I <bohdan.voiyov@gmail.com>
2023-07-16 15:31:46 +0300

    Task#3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import css from './App.module.css';
 
 import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics'
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 import user from 'data/user.json';
 import data from 'data/data.json';
-//import friends from 'data/friends.json';
+import friends from 'data/friends.json';
 //import transactions from 'data/transactions.json';
 
 export const App = () => {
@@ -18,7 +19,9 @@ export const App = () => {
         stats={user.stats}
         avatar={user.avatar}
       />
-      <Statistics title="Upload stats" stats={data} />
-    </div>
+      <Statistics title="Upload stats" stats={data} />   
+
+      <FriendList friends={friends} />
+    </div>    
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,18 @@
+import { FriendListItem } from './FriendListItem';
+import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({friends}) => {
+    return <ul className={css.friendList}>
+        {friends.map(({ avatar, name, isOnline, id }) => {
+            return <FriendListItem avatar={avatar} name={name} isOnline={isOnline} key={id} />;
+        })}
+         </ul>
+};
+
+FriendList.propTypes  = {
+    friends: PropTypes.arrayOf(
+        PropTypes.exact({
+            id:PropTypes.number.isRequired,
+        }))
+}
